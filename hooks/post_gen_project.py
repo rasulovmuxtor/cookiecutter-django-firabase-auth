d62a4520fad226a334140c61778de86b2c65d030
Mukhtor <rasulovoff@gmail.com>
2022-10-06 14:29:44 +0500

    fix: pre commit format

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -61,8 +61,7 @@ def remove_docker_files():
     for file_name in file_names:
         os.remove(file_name)
     if "{{ cookiecutter.use_pycharm }}".lower() == "y":
-        file_names = ["docker_compose_up_django.xml",
-                      "docker_compose_up_docs.xml"]
+        file_names = ["docker_compose_up_django.xml", "docker_compose_up_docs.xml"]
         for file_name in file_names:
             os.remove(os.path.join(".idea", "runConfigurations", file_name))
 
@@ -96,8 +95,7 @@ def remove_gulp_files():
 
 
 def remove_sass_files():
-    shutil.rmtree(
-        os.path.join("{{cookiecutter.project_slug}}", "static", "sass"))
+    shutil.rmtree(os.path.join("{{cookiecutter.project_slug}}", "static", "sass"))
 
 
 def remove_packagejson_file():
@@ -111,8 +109,7 @@ def remove_celery_files():
         os.path.join("config", "celery_app.py"),
         os.path.join("{{ cookiecutter.project_slug }}", "users", "tasks.py"),
         os.path.join(
-            "{{ cookiecutter.project_slug }}", "users", "tests",
-            "test_tasks.py"
+            "{{ cookiecutter.project_slug }}", "users", "tests", "test_tasks.py"
         ),
     ]
     for file_name in file_names:
@@ -141,8 +138,7 @@ def remove_dotgithub_folder():
 
 
 def generate_random_string(
-    length, using_digits=False, using_ascii_letters=False,
-    using_punctuation=False
+    length, using_digits=False, using_ascii_letters=False, using_punctuation=False
 ):
     """
     Example:
@@ -263,11 +259,9 @@ def append_to_gitignore_file(ignored_line):
 
 def set_flags_in_envs(postgres_user, celery_flower_user, debug=False):
     local_django_envs_path = os.path.join(".envs", ".local", ".django")
-    production_django_envs_path = os.path.join(".envs", ".production",
-                                               ".django")
+    production_django_envs_path = os.path.join(".envs", ".production", ".django")
     local_postgres_envs_path = os.path.join(".envs", ".local", ".postgres")
-    production_postgres_envs_path = os.path.join(".envs", ".production",
-                                                 ".postgres")
+    production_postgres_envs_path = os.path.join(".envs", ".production", ".postgres")
 
     set_django_secret_key(production_django_envs_path)
     set_django_admin_url(production_django_envs_path)
@@ -285,8 +279,7 @@ def set_flags_in_envs(postgres_user, celery_flower_user, debug=False):
     set_celery_flower_password(
         local_django_envs_path, value=DEBUG_VALUE if debug else None
     )
-    set_celery_flower_user(production_django_envs_path,
-                           value=celery_flower_user)
+    set_celery_flower_user(production_django_envs_path, value=celery_flower_user)
     set_celery_flower_password(
         production_django_envs_path, value=DEBUG_VALUE if debug else None
     )
@@ -317,24 +310,20 @@ def remove_aws_dockerfile():
 
 def remove_drf_starter_files():
     os.remove(os.path.join("config", "api_router.py"))
-    shutil.rmtree(
-        os.path.join("{{cookiecutter.project_slug}}", "users", "api"))
+    shutil.rmtree(os.path.join("{{cookiecutter.project_slug}}", "users", "api"))
     os.remove(
         os.path.join(
-            "{{cookiecutter.project_slug}}", "users", "tests",
-            "test_drf_urls.py"
+            "{{cookiecutter.project_slug}}", "users", "tests", "test_drf_urls.py"
         )
     )
     os.remove(
         os.path.join(
-            "{{cookiecutter.project_slug}}", "users", "tests",
-            "test_drf_views.py"
+            "{{cookiecutter.project_slug}}", "users", "tests", "test_drf_views.py"
         )
     )
     os.remove(
         os.path.join(
-            "{{cookiecutter.project_slug}}", "users", "tests",
-            "test_swagger.py"
+            "{{cookiecutter.project_slug}}", "users", "tests", "test_swagger.py"
         )
     )
 
@@ -344,8 +333,7 @@ def remove_rest_firebase_auth_starter_files():
 
 
 def remove_storages_module():
-    os.remove(
-        os.path.join("{{cookiecutter.project_slug}}", "utils", "storages.py"))
+    os.remove(os.path.join("{{cookiecutter.project_slug}}", "utils", "storages.py"))
 
 
 def main():
@@ -389,8 +377,8 @@ def main():
         if "{{ cookiecutter.keep_local_envs_in_vcs }}".lower() == "y":
             print(
                 INFO + ".env(s) are only utilized when Docker Compose and/or "
-                       "Heroku support is enabled so keeping them does not "
-                       "make sense given your current setup." + TERMINATOR
+                "Heroku support is enabled so keeping them does not "
+                "make sense given your current setup." + TERMINATOR
             )
         remove_envs_and_associated_files()
     else:
@@ -408,7 +396,7 @@ def main():
     if "{{ cookiecutter.cloud_provider}}" == "None":
         print(
             WARNING + "You chose not to use a cloud provider, "
-                      "media files won't be served in production." + TERMINATOR
+            "media files won't be served in production." + TERMINATOR
         )
         remove_storages_module()
 
